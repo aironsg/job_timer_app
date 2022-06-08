4d197616f13c4927bbba9d360a3cc880eb386d95
Airon silva <70911085+airon01@users.noreply.github.com>
2022-06-08 19:51:50 -0300

    Criando primeira parte da home page de projetos

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -3,34 +3,41 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:job_timer/app/core/database/database.dart';
 import 'package:job_timer/app/entities/project.dart';
 import 'package:job_timer/app/entities/project_status.dart';
+import 'package:job_timer/app/modules/home/widgets/header_project_menu.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Page'),
-      ),
-      body: Column(
-        children: [
-          Container(),
-          ElevatedButton(
-            onPressed: () async {
-              final db = Modular.get<Database>();
-              final connection = await db.openConnection();
-              await connection.writeTxn((isar) => connection.clear());
-              //abrindo uma transação
-              connection.writeTxn((isar) {
-                var project = Project();
-                project.name = 'teste';
-                project.status = ProjectStatus.em_andamento;
-                return connection.projects.put(project);
-              });
-            },
-            child: Text('Cadastrar'),
+      drawer: const Drawer(
+        child: SafeArea(
+          child: ListTile(
+            title: Text('Sair'),
           ),
-        ],
+        ),
+      ),
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            const SliverAppBar(
+              title: Text('Projetos'),
+              expandedHeight: 100,
+              toolbarHeight: 100,
+              centerTitle: true,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.circular(15),
+                ),
+              ),
+            ),
+            SliverPersistentHeader(
+              delegate: HeaderProjectMenu(),
+              pinned: true,
+            ),
+          ],
+        ),
       ),
     );
   }
